c1d5ea70f13665fb89499fd8fa745f2e9b7b24e5
s4704 <igorkud00@gmail.com>
2021-09-15 18:07:09 +0300

    BoardConfig.mk: Delete deprecated flags
    He need for patches.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,12 +2,6 @@ LOCAL_PATH := device/huawei/hn3_u00
 
 BOARD_VENDOR := Huawei
 
-USE_CAMERA_STUB := false
-
-# Audio
-TARGET_PROVIDES_LIBAUDIO := true
-BOARD_USES_GENERIC_AUDIO := false
-
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := k3v2oem1
@@ -15,20 +9,10 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := cortex-a9
-TARGET_ARCH_VARIANT_FPU := neon
 
 TARGET_BOOTLOADER_BOARD_NAME := hn3_u00
 TARGET_BOARD_PLATFORM := k3v2oem1
-
-# Local flag
-BOARD_USE_K3V2OEM1 := true
-
-# Webkit
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
 
 # Wifi
 USES_TI_MAC80211 := true
@@ -61,23 +45,10 @@ ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=1
 ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mass_storage
 
 # Graphics
-BOARD_EGL_CFG := $(LOCAL_PATH)/prebuilt/lib/egl/egl.cfg
 USE_OPENGL_RENDERER := true
-COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
-
-# Camera
-BOARD_CAMERA_HAVE_ISO := true
 
 # RIL
 BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril/
-
-# Misc
-BOARD_NEEDS_CUTILS_LOG := true
-BOARD_SCREENRECORD_DEVICE_FORCE_AUDIO_MIC := true
-TARGET_GRALLOC_USES_ASHMEM := true
-BOARD_USES_SECURE_SERVICES := true
-TARGET_USES_PMEM := true
-TARGET_USES_ION := false
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=k3v2oem1 androidboot.selinux=disabled vmalloc=384M k3v2_pmem=1 mmcparts=mmcblk0:p1(xloader),p3(nvme),p4(misc),p5(splash),p6(oeminfo),p7(reserved1),p8(reserved2),p9(splash2),p10(recovery2),p11(recovery),p12(boot),p13(modemimage),p14(modemnvm1),p15(modemnvm2),p16(system),p17(cache),p18(cust),p19(userdata);mmcblk1:p1(ext_sdcard)
